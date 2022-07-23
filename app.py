d715e401fb83b6ecc5b8f1b7c2899f775712606c
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-07-22 22:26:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
 import streamlit as st
 st.title('Club 3')
+
+#Seccion 1: Nuestro Club/Our Club
+my_page = st.sidebar.radio("Página de navegación", ["Biologia molecular/ Molecular Biology", "Proteinas/Proteins", "Resistencia antimicrobiana/Antimicrobial resistance", "COVID-19", "Referencias bibliográficas"])
