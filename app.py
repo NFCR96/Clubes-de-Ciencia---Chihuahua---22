393066bb6b4271cd7d87a75450a654146f39cf05
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-07-22 22:49:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 st.title('Club 3')
-st.write('en este club exploraremos posibilidades alternas para el desarollo de farmacos contra las enfermedades emergentes, basandonos en aproximaciones bioinformaticas')
+st.write('En este club exploraremos posibilidades alternas para el desarollo de farmacos novedosos contra infecciones virales y bacterianas, basandonos en aproximaciones bioinformaticas')
+st.write('In this club we are going to explore alternative possibilites for developing novel drugs against viral and bacterial infecitions based in bioinfrmatics approaches')  
 
 #Seccion 1: Nuestro Club/Our Club
 my_page = st.sidebar.radio("Página de navegación", ["Biologia molecular/ Molecular Biology", "Proteinas/Proteins", "Resistencia antimicrobiana/Antimicrobial resistance", "COVID-19", "Referencias bibliográficas"])
