f151d99da2993c5f88bb857e17ab043afd698590
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-08-06 11:35:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.write('En este club exploraremos posibilidades alternas para el desarollo de 
 st.write('In this club we are going to explore alternative possibilites for developing novel drugs against viral and bacterial infecitions based in bioinfrmatics approaches')  
 
 #Seccion 1: Nuestro Club/Our Club
-my_page = st.sidebar.radio("Página de navegación", ["Biologia molecular/ Molecular Biology", "Proteinas/Proteins", "Resistencia antimicrobiana/Antimicrobial resistance", "COVID-19", "Referencias bibliográficas"])
+my_page = st.sidebar.radio("Página de navegación", ["Resistencia antimicrobiana/Antimicrobial resistance", "Referencias bibliográficas"])
 st.write('')
 st.write('')
 st.write('')
